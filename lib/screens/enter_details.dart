9a793977c3b57b6ef9fe9f339bcb1c023b2ce67d
egorcherkasoff <egor-cherkasoff.ps@yandex.ru>
2024-02-05 21:11:19 +0800

    отрефакторил экран ввода доп данных после регистрации

--- lib/screens/enter_details.dart
+++ lib/screens/enter_details.dart
@@ -1,109 +1,131 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class EnterDetailsScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:psymetrica/screens/tabs.dart';
+
+class EnterDetailsScreen extends StatefulWidget {
   const EnterDetailsScreen({super.key});
 
-  void _pickImage() {
-    print('picking image...');
+  @override
+  State<EnterDetailsScreen> createState() => _EnterDetailsScreenState();
+}
+
+class _EnterDetailsScreenState extends State<EnterDetailsScreen> {
+  final _formKey = GlobalKey<FormState>();
+  // ignore: avoid_init_to_null
+  late XFile? _pickedImage = null;
+
+  Future<void> _pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? file = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _pickedImage = file;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              "Заполните данные о себе",
-              style: Theme.of(context).textTheme.titleLarge,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Заполните данные о себе",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              _detailsForm(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Form _detailsForm(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _avatarPicker(context),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            decoration: const InputDecoration(
+              hintText: "Введите ваше имя",
             ),
-            const SizedBox(
-              height: 8,
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            decoration: const InputDecoration(
+              hintText: "Введите вашу фамилию",
             ),
-            Form(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.all(8),
-                    width: 128,
-                    height: 128,
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black12, width: 1),
-                        borderRadius: BorderRadius.circular(15)),
-                    child: InkWell(
-                      splashColor: Theme.of(context).primaryColor,
-                      onTap: _pickImage,
-                    ),
-                  ),
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      label: Text("Введите ваш email",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium!
-                              .copyWith(color: Colors.black54, fontSize: 16)),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      contentPadding: const EdgeInsets.symmetric(
-                          vertical: 0, horizontal: 10),
-                    ),
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          fontSize: 16,
-                        ),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                      label: Text("Введите пароль",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium!
-                              .copyWith(color: Colors.black54, fontSize: 16)),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      contentPadding: const EdgeInsets.symmetric(
-                          vertical: 0, horizontal: 10),
-                    ),
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          fontSize: 16,
-                        ),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Theme.of(context).primaryColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                      child: Text(
-                        "Войти",
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyMedium!
-                            .copyWith(fontSize: 16, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ],
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            decoration: const InputDecoration(
+              hintText: "Введите ваше отчество",
+            ),
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(
+                  builder: (context) {
+                    return const TabsScreen();
+                  },
+                ), (route) => false);
+              },
+              style: Theme.of(context).elevatedButtonTheme.style,
+              child: const Text(
+                "Готово",
               ),
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _avatarPicker(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      margin: const EdgeInsets.all(8),
+      width: 275,
+      height: 275,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black12, width: 1),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: InkWell(
+        splashColor: Theme.of(context).primaryColor,
+        onTap: _pickImage,
+        child: _pickedImage != null
+            ? Image.file(
+                File(_pickedImage!.path),
+                fit: BoxFit.cover,
+              )
+            : Container(),
       ),
     );
   }
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -11,69 +11,96 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
-                  width: 128,
-                  height: 128,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black12, width: 1),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  clipBehavior: Clip.antiAlias,
-                  child: CachedNetworkImage(
-                    fit: BoxFit.cover,
-                    imageUrl:
-                        "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663657927_50-mykaleidoscope-ru-p-uspeshnii-psikholog-pinterest-56.jpg",
-                    placeholder: (context, url) => const SizedBox(
-                        width: 40,
-                        height: 40,
-                        child: CircularProgressIndicator()),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
-                  ),
-                ),
-                Text("Зыкова Надежда Николаевна",
-                    style: Theme.of(context).textTheme.bodyMedium),
-                Text(
-                  "Психолог",
-                  style: Theme.of(context).textTheme.labelMedium,
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  "Ваши тесты",
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const TestCard(
-                  test: Test(
-                    title: "Кто ты из слова пацана",
-                    description:
-                        "Этот тест позволит тебе наконец понять - кто ты из персонажей \"Слова пацана\"",
-                    questionsCount: 10,
-                    author: User(id: "asdada", fullName: "Надежда Авангард"),
-                    slug: "kto-tu-iz-slova-pacana",
-                    createdAt: "2024-01-01",
-                    id: "test_id",
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [_userInfo(context), _userTests(context)],
+        ),
       ),
+    );
+  }
+
+  Column _userTests(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          "Тесты пользователя",
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        const TestCard(
+          test: Test(
+              title: "Тест на дисперсию",
+              description:
+                  "Этот тест предназначен для тех, кто считает что у него депресия в 0 лет",
+              questionsCount: 38,
+              author: User(id: "asdada", fullName: "Надежда Авангард"),
+              slug: "test-dispersia",
+              createdAt: "2024-01-01",
+              id: "test_id1"),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        const TestCard(
+          test: Test(
+              title: "Тест на дисперсию",
+              description:
+                  "Этот тест предназначен для тех, кто считает что у него депресия в 0 лет",
+              questionsCount: 38,
+              author: User(id: "asdada", fullName: "Надежда Авангард"),
+              slug: "test-dispersia",
+              createdAt: "2024-01-01",
+              id: "test_id1"),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        const TestCard(
+          test: Test(
+              title: "Тест на дисперсию",
+              description:
+                  "Этот тест предназначен для тех, кто считает что у него депресия в 0 лет",
+              questionsCount: 38,
+              author: User(id: "asdada", fullName: "Надежда Авангард"),
+              slug: "test-dispersia",
+              createdAt: "2024-01-01",
+              id: "test_id1"),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+      ],
+    );
+  }
+
+  Column _userInfo(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Center(
+          child: Container(
+            clipBehavior: Clip.hardEdge,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(8),
+              ),
+            ),
+            child: Image.network(
+              "https://img.goodfon.ru/original/1280x1024/a/4b/3d-geometric-cube.jpg",
+              fit: BoxFit.cover,
+              width: 200,
+              height: 200,
+            ),
+          ),
+        ),
+        Text(
+          "Надежда Авангард",
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        Text("Психолог"),
+      ],
     );
   }
 }
